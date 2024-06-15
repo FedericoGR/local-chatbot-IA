bceddaa257dfab23cc1c5086e2189c113384cb28
FedericoGR <fedegr77@gmail.com>
2024-06-15 01:37:19 -0300

    Features: Added the Llama-3 model and chatbot functionality. Implemented the WebWorkers. Changed the UI. Updated the Readme.

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Web-LLM Chatbot
 
-This is a simple HTML/JS/CSS starter template for building a ChatGPT-like chatbot using the Web-LLM library and TinyLlama v1.0.
+This is a simple HTML/JS/CSS template for building a ChatGPT-like chatbot using the Web-LLM library and Llama-3 8B.
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+## Features
+
+- **Local Processing**: Runs entirely in your browser, ensuring the privacy.
+- **Llama-3 8B**: Open source AI generative model.
+
+<img alt="an image preview of the chatbot" height="500" style="display:block; margin:0 auto; border-radius: 2%;" src="./chatbot-preview.png">
 
 ## Usage
 
@@ -8,15 +19,11 @@ To use the chatbot, simply type a message into the input field and press Enter. 
 
 ## **Important Note:**
 
-This chatbot runs locally in your browser, which can be computationally expensive. Please be mindful of your PC's resources.
+This chatbot runs locally in your browser using your GPU as a main source of computation the vram required can be as high as 5295.7 on this model. Please be mindful of your PC's resources.
 
-I've used a smaller version of Llama (TinyLlama v1.0) to mitigate this issue. However, if you're looking for more options, you can check out the full list of available models on the Web-LLM website and swap them out as needed.
+However, if you're looking for more options, you can check out the full list of available models on the [Web-LLM repo](https://github.com/mlc-ai/web-llm) and swap them out as needed.
 
 ## Credits
 
-* [Web-LLM library](https://github.com/mlc-ai/web-llm) 
-* [midudev](https://www.youtube.com/@midudev) for teaching us how to do this in the first place
-
-
-
-
+- [Web-LLM library](https://github.com/mlc-ai/web-llm)
+- [midudev](https://www.youtube.com/@midudev) for teaching us how to do this in the first place
--- main.js
+++ main.js
@@ -1,53 +1,92 @@
-import { CreateMLCEngine } from "https://esm.run/@mlc-ai/web-llm"
+import { CreateWebWorkerMLCEngine } from 'https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm'
 
-const SELECTED_MODEL = 'TinyLlama-1.1B-Chat-v1.0-q4f16_1-MLC';
-const engine = await CreateMLCEngine(SELECTED_MODEL,
+const $ = (el) => document.querySelector(el)
+
+const $form = $('form')
+const $input = $('form input')
+const $messages = $('ul')
+const $template = $('#message-template')
+const $button = $('button')
+const $container = $('main')
+const $info = $('small')
+const $loading = $('.loading')
+
+let messages = []
+let end = false
+
+const SELECTED_MODEL = 'Llama-3-8B-Instruct-q4f32_1-MLC-1k'
+
+const engine = await CreateWebWorkerMLCEngine(
+  new Worker('./worker.js', { type: 'module' }),
+  SELECTED_MODEL,
   {
     initProgressCallback: (info) => {
-      console.log('Progress',info);
+      $info.textContent = `${info.text}`
+      if (info.progress === 1 && !end) {
+        end = true
+        $loading?.parentNode?.removeChild($loading)
+        $button.removeAttribute('disabled')
+        addMessage(
+          'Welcome! This is an AI assistant. You can ask me anything you want. I am going to try my best to answer. I am powered with Llama-3.',
+          'bot'
+        )
+        $input.focus()
+      }
+    },
+  }
+)
+
+$form.addEventListener('submit', async (e) => {
+  e.preventDefault()
+
+  const messageText = $input.value.trim()
+
+  if (messageText !== '') {
+    $input.value = ''
+
+    addMessage(messageText, 'user')
+    $button.setAttribute('disabled', '')
+
+    const userMessage = { role: 'user', content: messageText }
+    messages.push(userMessage)
+
+    const chunks = await engine.chat.completions.create({
+      messages,
+      stream: true,
+    })
+
+    let reply = ''
+
+    const $botMessage = addMessage('', 'bot')
+
+    for await (const chunk of chunks) {
+      const choice = chunk.choices[0]
+      const content = choice?.delta?.content ?? ''
+
+      reply += content
+      $botMessage.textContent = reply
     }
-  });
 
-
-const $ = el => document.querySelector(el);
-
-const $form = $('form');
-const $input = $('form input');
-const $messages = $('ul');
-const $template = $('#message-template');
-const $button = $('button');
-const $container = $('main');
-
-$form.addEventListener('submit', (e) => {
-  e.preventDefault(); 
-
-const messageText = $input.value.trim();
-
-if (messageText !== '') {
-  $input.value = ''
-
-  addMessage(messageText, 'user');
-  $button.setAttribute('disabled', true);
-
-  setTimeout(() => {
-    addMessage('Respuesta del bot', 'bot');
-    $button.removeAttribute('disabled');
-  }, 1000);
-
-}
+    $button.removeAttribute('disabled')
+    messages.push({
+      role: 'assistant',
+      content: reply,
+    })
+    $container.scrollTop = $container.scrollHeight
+  }
+})
 
 function addMessage(text, sender) {
-  const clonedTemplate = $template.content.cloneNode(true);
+  const clonedTemplate = $template.content.cloneNode(true)
   const $newMessage = clonedTemplate.querySelector('.message')
-  const $who = $newMessage.querySelector('span');
-  const $text= $newMessage.querySelector('p');
+  const $who = $newMessage.querySelector('span')
+  const $text = $newMessage.querySelector('p')
 
-  $text.textContent = text;
-  $who.textContent = sender === 'bot' ? 'GPT' : 'User';
-  $newMessage.classList.add(sender);
-  $messages.appendChild($newMessage);
-  $container.scrollTop = $container.scrollHeight;
+  $text.textContent = text
+  $who.textContent = sender === 'bot' ? 'ChatBot' : 'User'
+  $newMessage.classList.add(sender)
+  $messages.appendChild($newMessage)
+  $container.scrollTop = $container.scrollHeight
+
+  return $text
 }
-
-
-});
